80f4fe4a76c1a3c96a2106cef5575d901f3740bd
Shota Fukumori (sora_h) <her@sorah.jp>
2013-07-21 03:55:57 +0900

    Add some specs for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,54 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { described_class.new }
+
+  describe "#staff?" do
+    subject { user.staff? }
+
+    context "if staff status isn't present" do
+      before { user.staff = nil }
+      it { should be_false }
+    end
+
+    context "if #staff is false" do
+      before { user.staff = false }
+      it { should be_false }
+    end
+
+    context "if #staff is true" do
+      before { user.staff = true }
+      it { should be_true }
+    end
+  end
+
+  describe "#shown_name" do
+    pending "WRITE!"
+  end
+
+  describe "#promote!" do
+    before do
+      user.staff = false
+    end
+
+    it "promotes itself to staff" do
+      expect { user.promote! } \
+        .to change { user.staff? } \
+        .from(false).to(true)
+    end
+  end
+
+  describe "#demote!" do
+    before do
+      user.staff = true
+    end
+
+    it "demotes itself from staff" do
+      expect { user.demote! } \
+        .to change { user.staff? } \
+        .from(true).to(false)
+    end
+  end
+
+  after(:all) { User.destroy_all }
 end

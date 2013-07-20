305d13ab2979e0e453e95548a49a2f7a30186b40
Shota Fukumori (sora_h) <her@sorah.jp>
2013-07-21 03:55:29 +0900

    User#demote!, promote!: for change staff flag

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   def staff?() self.staff end
   def shown_name() self.name || self.nick end
+
+  def promote!
+    self.update_attributes!(staff: true)
+  end
+
+  def demote!
+    self.update_attributes!(staff: false)
+  end
 end

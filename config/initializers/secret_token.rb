930281325594789d4e593a249c5d3fc6d68ab0b1
Shota Fukumori (sora_h) <her@sorah.jp>
2013-07-20 19:17:09 +0900

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coliseum::Application.config.secret_key_base = '8540df825b6e1e7cf2af5322c9f94112dcd48a43940814e57fa7460794e34521769bcdafdce9149fccc37e2aa515f7a5dc41b89c84a90394ccc189c2e46ea83b'

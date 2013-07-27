b47ebfca42832441ca6fc8600c935931ccbf0872
Shota Fukumori (sora_h) <her@sorah.jp>
2013-07-27 14:14:36 +0900

    Submission: Support streaming using Redis
    * Submission#stream: subscribe on Redis, yields recieved submissions
    * Submission#judge!: Announce on redis after saving
    * Submission#kick_judge: Announce on redis

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,12 +1,31 @@
 require 'judge'
 
 class Submission < ActiveRecord::Base
+  REDIS_CHANNEL = 'coliseum:submission'
+
   belongs_to :user
   belongs_to :problem
 
   serialize :judge_result, Array
 
+  class << self
+    def stream
+      Redis.current.subscribe(REDIS_CHANNEL) do |on|
+        on.message do |ch, id|
+          begin
+            submission = Submission.find(id)
+          rescue ActiveRecord::RecordNotFound
+            next
+          end
+
+          yield submission
+        end
+      end
+    end
+  end
+
   def kick_judge
+    announce
     JudgeWorker.perform_async(self.id)
   end
   after_create :kick_judge
@@ -20,6 +39,8 @@ class Submission < ActiveRecord::Base
     self.judge_result = nil
     self.save!
 
+    announce
+
     self.judge_result = self.problem.test_examples.map do |test_example|
       Judge.new(test_example).test(self.code)
     end
@@ -30,5 +51,15 @@ class Submission < ActiveRecord::Base
                           :failed
                         end
     self.save!
+
+    announce
+
+    self
+  end
+
+  private
+
+  def announce
+    Redis.current.publish(REDIS_CHANNEL, self.id.to_s)
   end
 end

a588dad93874f70fb3729954812e12786e4dd929
Shota Fukumori (sora_h) <her@sorah.jp>
2014-01-18 16:19:36 +0900

    [debug] Retry patiently even if record not found

--- app/workers/judge_worker.rb
+++ app/workers/judge_worker.rb
@@ -2,10 +2,14 @@ class JudgeWorker
   include Sidekiq::Worker
 
   def perform(submission_id)
+    retries = 0
     begin
       submission = Submission.find(submission_id.to_i)
     rescue ActiveRecord::RecordNotFound
-      return nil
+      return nil if 5 < retries 
+      retries += 1
+      p "Retried #{submission_id} (#{retries})"
+      retry
     end
 
     submission.judge!
